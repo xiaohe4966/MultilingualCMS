4bbfa25db4c4b21fc8aa0d02efec73372d7389d0
xiaohe4966 <496631085@qq.com>
2024-12-04 15:12:25 +0800

    添加后台接口

---
+++ application/admin/controller/Api.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * @Author: he4966
+ */
+/*
+ * @Author: XiaoHe
+ */
+
+namespace app\admin\controller;
+
+use app\common\controller\Backend;
+use think\Db;
+
+/**
+ * 违禁词
+ *
+ * @icon fa fa-circle-o
+ */
+class Api extends Backend
+{
+
+    // /**
+    //  * Bans模型对象
+    //  * @var \app\admin\model\Bans
+    //  */
+    // protected $model = null;
+
+    public function _initialize()
+    {
+        parent::_initialize();
+        // $this->model = new \app\admin\model\Bans;
+    }
+
+
+    /**
+     * 同步栏目翻译其他语言
+     */
+    public function fanyi_cate($ids=null){
+       $LangCom = new \app\common\library\LangCom();
+       $size = $LangCom->fanyiCate($ids);
+
+       $this->success('同步完成,共翻译'.$size.'条数据');
+    }
+
+    /**
+     * 翻译所有栏目
+     */
+    public function fanyi_all_cate(){
+       $LangCom = new \app\common\library\LangCom();
+       $LangCom->updateCate();
+
+       $this->success('同步完成,共插入'.$LangCom::$insertNum.'条栏目');
+    }
+}
