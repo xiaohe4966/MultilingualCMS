aae5f51521f6292d25214a6d8c163c720ea41246
xiaohe4966 <496631085@qq.com>
2024-12-11 14:05:27 +0800

    后台添加富文本编辑器

--- addons/summernote/bootstrap.js
+++ addons/summernote/bootstrap.js
@@ -1,31 +1,3 @@
-define([], function () {
-    require.config({
-    paths: {
-        'editable': '../libs/bootstrap-table/dist/extensions/editable/bootstrap-table-editable.min',
-        'x-editable': '../addons/editable/js/bootstrap-editable.min',
-    },
-    shim: {
-        'editable': {
-            deps: ['x-editable', 'bootstrap-table']
-        },
-        "x-editable": {
-            deps: ["css!../addons/editable/css/bootstrap-editable.css"],
-        }
-    }
-});
-if ($("table.table").length > 0) {
-    require(['editable', 'table'], function (Editable, Table) {
-        $.fn.bootstrapTable.defaults.onEditableSave = function (field, row, oldValue, $el) {
-            var data = {};
-            data["row[" + field + "]"] = row[field];
-            Fast.api.ajax({
-                url: this.extend.edit_url + "/ids/" + row[this.pk],
-                data: data
-            });
-        };
-    });
-}
-
 require.config({
     paths: {
         'summernote': '../addons/summernote/lang/summernote-zh-CN.min'
@@ -136,6 +108,4 @@ require(['form', 'upload'], function (Form, Upload) {
         }
 
     };
-});
-
 });
