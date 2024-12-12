f68696434288241f7d05b3fca1dbbe669d98e810
xiaohe4966 <496631085@qq.com>
2024-12-12 16:58:39 +0800

    多语言配置存到数据库语言

---
+++ application/admin/lang/zh-cn/dbconfig.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'Name'         => '变量名',
+    'Group'        => '分组',
+    'Title'        => '变量标题',
+    'Tip'          => '变量描述',
+    'Type'         => '类型:string,text,int,bool,array,datetime,date,file',
+    'Visible'      => '可见条件',
+    'Value'        => '变量值',
+    'Content'      => '变量字典数据',
+    'Rule'         => '验证规则',
+    'Extend'       => '扩展属性',
+    'Setting'      => '配置',
+    'Weigh'        => '权重',
+    'Lang'         => '语言版本',
+    'Fanyi_switch' => '是否翻译',
+    'Fanyi_sha1'   => '语言sha1',
+    'Fanyi_time'   => '翻译时间',
+    'Fanyi_num'    => '翻译次数',
+    'Copy_id'      => '复制ID'
+];
