b7606d2d2851e7e62e5b97464c25f33eb3dced42
xiaohe4966 <496631085@qq.com>
2024-12-13 09:30:35 +0800

    限制翻译速度 判断缓存

--- extend/fast/BaiduTranslate.php
+++ extend/fast/BaiduTranslate.php
@@ -2,29 +2,26 @@
 /*
  * @Author: he4966
  */
-/*
- * @Author: he4966
- */
-/*
- * @Author: he4966
- */
-// application/BaiduTranslate.php
 
 namespace fast;
 
 
 use think\Exception;
 use think\Config;
+use think\Cache;
+
 class BaiduTranslate
 {
     protected $appId;
     protected $appSecret;
     protected $apiUrl;
+    protected $QPS;
 
-    public function __construct($appId=null, $appSecret=null)
+    public function __construct($appId = null, $appSecret = null, $QPS = null)
     {
-        $this->appId = $appId??Config::get('site.baidu_app_id');
-        $this->appSecret = $appSecret??Config::get('site.baidu_app_secret');
+        $this->appId = $appId ?? Config::get('site.baidu_app_id');
+        $this->appSecret = $appSecret ?? Config::get('site.baidu_app_secret');
+        $this->QPS = $QPS ?? Config::get('site.baidu_app_qps');
         $this->apiUrl = "https://api.fanyi.baidu.com/api/trans/vip/translate";
     }
 
@@ -37,6 +34,24 @@ class BaiduTranslate
      */
     public  function translate($query, $from = 'auto', $to = 'zh')
     {
+        //限制速度 判断用缓存
+        $cacheKey = 'baidu_translate_'.$this->QPS;
+        $num = Cache::get($cacheKey);
+        if ($num) {
+            if($this->QPS<=$num){
+                while (true) {
+                    $num = Cache::get($cacheKey);
+                    if(!$num){
+                        break;
+                    }
+                    sleep(0.1);
+                }
+            }
+        }else{
+            $num = 0;
+        }
+        Cache::set($cacheKey, $num + 1, 1);
+
         $salt = rand(10000, 99999);
         $sign = md5($this->appId . $query . $salt . $this->appSecret);
 
@@ -49,7 +64,7 @@ class BaiduTranslate
             'sign'  => $sign,
         ];
         // halt($params);
-     
+
         try {
             $result = \fast\Http::post($this->apiUrl, $params);
             $result = json_decode($result, true);
@@ -59,7 +74,7 @@ class BaiduTranslate
                 throw new Exception("翻译失败：" . $result['error_msg']);
             }
         } catch (Exception $e) {
-            throw new Exception("翻译错误：" . $e->getMessage().json_encode($params,JSON_UNESCAPED_UNICODE));
+            throw new Exception("翻译错误：" . $e->getMessage() . json_encode($params, JSON_UNESCAPED_UNICODE));
         }
     }
 }
